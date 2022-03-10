0e111d90655bc331102f521496659629735fa896
Unbound <f1arenova1@gmail.com>
2022-03-10 18:42:32 +0300

    change schema

--- src/api/interface/controllers/interface.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  interface controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::interface.interface');
--- src/api/interface/services/interface.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * interface service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::interface.interface');
